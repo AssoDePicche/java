2fc30a063d0c98f0949b4ab2c7b40f5b0b65b84d
AssoDePicche <samuelprado730@gmail.com>
2023-01-15 22:40:47 -0300

    Feat: Add Java Exception Handling (Try-catch)

---
+++ code/Exception Handling/Java Exception Handling (Try-catch)/Solution.java
@@ -0,0 +1,25 @@
+import java.util.InputMismatchException;
+
+import java.util.Scanner;
+
+public class Solution {
+  public static void main(String[] args) {
+    Scanner scanner = new Scanner(System.in);
+
+    int x, y;
+
+    try {
+      x = scanner.nextInt();
+
+      y = scanner.nextInt();
+
+      System.out.println(x / y);
+    } catch (InputMismatchException e) {
+      System.out.println("java.util.InputMismatchException");
+    } catch (Exception exception) {
+      System.out.println(exception);
+    } finally {
+      scanner.close();
+    }
+  }
+}
