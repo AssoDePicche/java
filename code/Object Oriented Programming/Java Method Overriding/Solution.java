62c8bb77154eca02f38bd9858962bd8446d0b774
AssoDePicche <samuelprado730@gmail.com>
2023-01-16 08:53:42 -0300

    Feat: Add Java Method Overriding

---
+++ code/Object Oriented Programming/Java Method Overriding/Solution.java
@@ -0,0 +1,17 @@
+import java.lang.reflect.Method;
+
+public class Solution {
+  public static void main(String[] args) {
+    Sports sports = new Sports();
+
+    Soccer soccer = new Soccer();
+
+    System.out.println(sports.getName());
+
+    sports.getNumberOfTeamMembers();
+
+    System.out.println(soccer.getName());
+
+    soccer.getNumberOfTeamMembers();
+  }
+}
