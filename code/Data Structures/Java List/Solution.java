a77b550cb1a2d7501deac4639eea90951d144d39
AssoDePicche <samuelprado730@gmail.com>
2023-01-18 23:19:50 -0300

    Feat: Add Java List

---
+++ code/Data Structures/Java List/Solution.java
@@ -0,0 +1,57 @@
+import java.util.ArrayList;
+
+import java.util.Scanner;
+
+public class Solution {
+  public static void main(String[] args) {
+    ArrayList<Integer> list = new ArrayList<>();
+
+    Scanner scanner = new Scanner(System.in);
+
+    int elements, queries, x, y;
+
+    String command;
+
+    elements = scanner.nextInt();
+
+    while (elements != 0) {
+      x = scanner.nextInt();
+
+      list.add(x);
+
+      elements--;
+    }
+
+    queries = scanner.nextInt();
+
+    while (queries != 0) {
+      command = scanner.nextLine();
+
+      switch (command.toLowerCase()) {
+        case "insert":
+          x = scanner.nextInt();
+
+          y = scanner.nextInt();
+
+          list.add(x, y);
+
+          queries--;
+          break;
+
+        case "delete":
+          x = scanner.nextInt();
+
+          list.remove(x);
+
+          queries--;
+          break;
+      }
+    }
+
+    scanner.close();
+
+    for (int item : list) {
+      System.out.printf("%d ", item);
+    }
+  }
+}
