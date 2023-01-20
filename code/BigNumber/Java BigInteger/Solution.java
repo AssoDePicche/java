c51a73ddaaf7c8db68646436c897203412e05289
AssoDePicche <samuelprado730@gmail.com>
2023-01-19 23:52:18 -0300

    Feat: Add Java BigInteger

---
+++ code/BigNumber/Java BigInteger/Solution.java
@@ -0,0 +1,21 @@
+import java.math.BigInteger;
+
+import java.util.Scanner;
+
+public class Solution {
+  public static void main(String[] args) {
+    BigInteger a, b;
+
+    Scanner scanner = new Scanner(System.in);
+
+    a = scanner.nextBigInteger();
+
+    b = scanner.nextBigInteger();
+
+    scanner.close();
+
+    System.out.println(a.add(b));
+
+    System.out.println(a.multiply(b));
+  }
+}
