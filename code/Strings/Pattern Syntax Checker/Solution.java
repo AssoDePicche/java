ed9548d42071da90c5abb569efc409fa83751303
AssoDePicche <samuelprado730@gmail.com>
2023-01-19 23:47:37 -0300

    Feat: Add Pattern Syntax Checker

---
+++ code/Strings/Pattern Syntax Checker/Solution.java
@@ -0,0 +1,27 @@
+import java.util.regex.Pattern;
+
+import java.util.regex.PatternSyntaxException;
+
+import java.util.Scanner;
+
+public class Solution {
+  public static void main(String[] args) {
+    Scanner scanner = new Scanner(System.in);
+
+    Integer n = Integer.parseInt(scanner.nextLine());
+
+    while (n-- != 0) {
+      String pattern = scanner.nextLine();
+
+      try {
+        Pattern.compile(pattern);
+
+        System.out.println("Valid");
+      } catch (PatternSyntaxException e) {
+        System.out.println("Invalid");
+      }
+    }
+
+    scanner.close();
+  }
+}
