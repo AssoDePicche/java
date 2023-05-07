da1e939e28c6c2564b959e3c853a691f71972efd
AssoDePicche <samuelprado730@gmail.com>
2023-05-07 18:12:00 -0300

    refactor: extract number validation to a class

--- src/Introduction/Java Datatypes/Solution.java
+++ src/Introduction/Java Datatypes/Solution.java
@@ -2,33 +2,29 @@ import java.util.Scanner;
 
 public class Solution {
   public static void main(String[] args) {
-    int testCases;
-
-    long number;
-
     Scanner scanner = new Scanner(System.in);
 
-    testCases = scanner.nextInt();
+    int testCases = scanner.nextInt();
 
     while (testCases-- != 0) {
       try {
-        number = scanner.nextLong();
+        long number = scanner.nextLong();
 
         System.out.println(number + " can be fitted in:");
 
-        if (number >= Byte.MIN_VALUE && number <= Byte.MAX_VALUE) {
+        if (NumberMeter.fitsInOneByte(number)) {
           System.out.println("* byte");
         }
 
-        if (number >= Short.MIN_VALUE && number <= Short.MAX_VALUE) {
+        if (NumberMeter.fitsInOneShort(number)) {
           System.out.println("* short");
         }
 
-        if (number >= Integer.MIN_VALUE && number <= Integer.MAX_VALUE) {
+        if (NumberMeter.fitsInOneInteger(number)) {
           System.out.println("* int");
         }
 
-        if (number >= Long.MIN_VALUE && number <= Long.MAX_VALUE) {
+        if (NumberMeter.fitsInOneLong(number)) {
           System.out.println("* long");
         }
       } catch (Exception exception) {
