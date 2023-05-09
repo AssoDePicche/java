fb7a4c544cf7d3db0ab22c1447a6f9b24c3f0c15
AssoDePicche <samuelprado730@gmail.com>
2023-05-09 01:10:02 -0300

    refactor: improve code design

--- src/Introduction/Java Output Formatting/Solution.java
+++ src/Introduction/Java Output Formatting/Solution.java
@@ -1,43 +1,20 @@
 import java.util.Scanner;
 
 public class Solution {
+  private static final String OUTPUT_FORMAT = "%-14s %03d\n", LINE = "=".repeat(32);
+
+  private static final int ENTRIES = 3;
   public static void main(String[] args) {
     Scanner scanner = new Scanner(System.in);
 
-    System.out.println("================================");
+    System.out.println(LINE);
 
-    for (int i = 0; i < 3; i++) {
-      String stringInput;
-
-      int count, integerInput, necessarySpaces;
-
-      stringInput = scanner.next();
-
-      integerInput = scanner.nextInt();
-
-      necessarySpaces = 18 - (stringInput.length() + 3);
-
-      count = 0;
-
-      while (count < necessarySpaces) {
-        stringInput += " ";
-
-        count++;
-      }
-
-      if (integerInput < 10) {
-        stringInput += "00";
-      }
-
-      if (integerInput >= 10 && integerInput < 100) {
-        stringInput += "0";
-      }
-
-      System.out.printf("%s%d\n", stringInput, integerInput);
+    for (int count = 0; count < ENTRIES; count++) {
+      System.out.printf(OUTPUT_FORMAT, scanner.next(), scanner.nextInt());
     }
 
-    System.out.println("================================");
-
     scanner.close();
+
+    System.out.println(LINE);
   }
 }
