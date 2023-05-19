2a671b563e30a8329dd27da101568f84ccf15f80
AssoDePicche <samuelprado730@gmail.com>
2023-05-18 22:33:18 -0300

    refactor: improve code readability

--- src/Exception Handling/Java Exception Handling/MyCalculator.java
+++ src/Exception Handling/Java Exception Handling/MyCalculator.java
@@ -1,13 +1,11 @@
 public class MyCalculator {
-  public long power(int n, int p) throws Exception {
-    if (n < 0 || p < 0) {
+  public static long power(int base, int exponent) throws Exception {
+    if (base < 0 || exponent < 0) {
       throw new Exception("n or p should not be negative.");
-    }
-
-    if (n == 0 && p == 0) {
+    } else if (base == 0 && exponent == 0) {
       throw new Exception("n and p should not be zero.");
     }
 
-    return (long) Math.pow(n, p);
+    return (long) Math.pow(base, exponent);
   }
 }
